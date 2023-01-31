55dc74d99e4a7e86e03e38e355115f855a9082fc
Kunuk Nykjær <knyk@novonordisk.com>
2023-01-31 14:39:40 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,33 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
-    
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/lib/main.ts'),
-      name: 'VueCollapse',
+      entry: resolve(__dirname, "src/lib/main.ts"),
+      name: "VueCollapse",
       // the proper extensions will be added
-      fileName: 'vue-collapse',
+      fileName: "vue-collapse",
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
         },
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') return 'vue-collapse.css';
+          if (assetInfo.name === "style.css") return "vue-collapse.css";
           return assetInfo.name;
         },
       },
     },
   },
-})
+});
