21f24cf9b54b6f2354dd2e044c8b4985bd741ca3
Kunuk Nykjær <knyk@novonordisk.com>
2023-06-25 11:57:44 +0200

    add files

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+export default {
+  verbose: true,
+  injectGlobals: true,
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'js', 'json', 'vue'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)?$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  setupFiles: ['<rootDir>/tests/setup'],
+  coverageDirectory: '<rootDir>/tests/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/locale/translations/**/*.js',
+  ],
+}
