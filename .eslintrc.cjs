85172c276fb920c96200727a5abfce36cbcf9b1a
kunukn <kunukn@gmail.com>
2024-10-13 13:31:24 +0200

    updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
